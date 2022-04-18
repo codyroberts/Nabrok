fc36a778a0dccef3b2f5b16b00bd8e2502b6a47a
quin-thinkbean <101130109+quin-thinkbean@users.noreply.github.com>
2022-04-17 22:15:12 -0500

    fixed animation; formatting

--- src/animations/player.ts
+++ src/animations/player.ts
@@ -1,40 +1,65 @@
 import * as Phaser from 'phaser'
 
 function playerAnimation(scene: Phaser.Scene) {
-    scene.anims.create({
-        key: 'idle',
-        frames: scene.anims.generateFrameNumbers('player_idle', { start: 0, end: 15}),
-        frameRate: 5,
-        repeat: -1
-    });
+  scene.anims.create({
+    key: 'idle',
+    frames: scene.anims.generateFrameNumbers('player_idle', {
+      start: 0,
+      end: 15,
+    }),
+    frameRate: 5,
+    repeat: -1,
+  })
 
-      scene.anims.create({
-        key: 'walk_down',
-        frames: scene.anims.generateFrameNumbers('player_walk', {start: 0, end: 3}),
-        frameRate: 5,
-        repeat: -1
-      })
+  scene.anims.create({
+    key: 'walk_down',
+    frames: scene.anims.generateFrameNumbers('player_walk', {
+      start: 0,
+      end: 3,
+    }),
+    frameRate: 5,
+    repeat: -1,
+  })
 
-      scene.anims.create({
-        key: 'walk_up',
-        frames: scene.anims.generateFrameNumbers('player_walk', {start: 9, end: 11}),
-        frameRate: 5,
-        repeat: -1
-      })
+  scene.anims.create({
+    key: 'walk_up',
+    frames: scene.anims.generateFrameNumbers('player_walk', {
+      start: 9,
+      end: 11,
+    }),
+    frameRate: 5,
+    repeat: -1,
+  })
 
-      scene.anims.create({
-        key: 'walk_sideways',
-        frames: scene.anims.generateFrameNumbers('player_walk', {start: 0, end: 3}),
-        frameRate: 5,
-        repeat: -1
-      })
+  scene.anims.create({
+    key: 'walk_sideways',
+    frames: scene.anims.generateFrameNumbers('player_walk', {
+      start: 0,
+      end: 3,
+    }),
+    frameRate: 5,
+    repeat: -1,
+  })
+  console.log(
+    scene.anims.generateFrameNumbers('player_attack', {
+      start: 0,
+      end: 4,
+    })
+  )
+  console.log(
+    scene.anims.generateFrameNumbers('player_attack', {
+      start: 2,
+      end: 5,
+    })
+  )
 
-      scene.anims.create({
-          key: 'attack',
-          frames: scene.anims.generateFrameNumbers('player_attack', {end: 3}),
-          frameRate: 5,
-          repeat: -1
-      })
+  scene.anims.create({
+    key: 'attack',
+    frames: scene.anims.generateFrameNumbers('player_attack', {
+      end: 5,
+    }),
+    frameRate: 5,
+  })
 }
 
 export default playerAnimation
--- src/controls.ts
+++ src/controls.ts
@@ -2,59 +2,61 @@
 
 import * as Phaser from 'phaser'
 
-function controls (scene: Phaser.Scene, player:Phaser.Physics.Arcade.Sprite) {
-    const controls = scene.input.keyboard.createCursorKeys();
-    const body = player.body as Phaser.Physics.Arcade.Body
+let attacking = false
+function controls(scene: Phaser.Scene, player: Phaser.Physics.Arcade.Sprite) {
+  const controls = scene.input.keyboard.createCursorKeys()
+  const body = player.body as Phaser.Physics.Arcade.Body
 
-      // TODO: Cut controls out into function. CLean up function with fewer if statements.
-      if(!controls.up.isDown && !controls.down.isDown && !controls.left.isDown && !controls.right.isDown){
-        player.anims.play('idle', true);
-      }
+  // TODO: Cut controls out into function. CLean up function with fewer if statements.
+  if (
+    !controls.up.isDown &&
+    !controls.down.isDown &&
+    !controls.left.isDown &&
+    !controls.right.isDown &&
+    !controls.space.isDown
+  ) {
+    player.anims.play('idle', true)
+  }
 
-      if(controls.up.isDown){
-        body.setVelocityY(-45);
-        player.anims.play('walk_up', true);
-      }
-      else if(controls.down.isDown){
-        body.setVelocityY(45);
-        player.anims.play('walk_down', true);
-      }
-      else {
-        body.setVelocityY(0);
-      }
+  if (controls.up.isDown) {
+    body.setVelocityY(-45)
+    player.anims.play('walk_up', true)
+  } else if (controls.down.isDown) {
+    body.setVelocityY(45)
+    player.anims.play('walk_down', true)
+  } else {
+    body.setVelocityY(0)
+  }
 
-      if(controls.left.isDown){
-        body.setVelocityX(-45);
-        if(controls.down.isDown) {
-          player.anims.play('walk_down', true);
-        }
-        else if(controls.up.isDown) {
-          player.anims.play('walk_up', true)
-        } 
-        else {
-          player.flipX = true;
-          player.anims.play('walk_sideways', true);
-        }
-      }
-      else if(controls.right.isDown){
-        body.setVelocityX(45);
-        if(controls.down.isDown) {
-          player.anims.play('walk_down', true);
-        }
-        else if(controls.up.isDown) {
-          player.anims.play('walk_up', true)
-        } 
-        else {
-          player.flipX = false;
-          player.anims.play('walk_sideways', true);
-        }
-      }
-      else {
-        body.setVelocityX(0);
-      }
-      if (controls.space.isDown) {
-          player.anims.play('attack', true)
-      }
+  if (controls.left.isDown) {
+    body.setVelocityX(-45)
+    if (controls.down.isDown) {
+      player.anims.play('walk_down', true)
+    } else if (controls.up.isDown) {
+      player.anims.play('walk_up', true)
+    } else {
+      player.flipX = true
+      player.anims.play('walk_sideways', true)
+    }
+  } else if (controls.right.isDown) {
+    body.setVelocityX(45)
+    if (controls.down.isDown) {
+      player.anims.play('walk_down', true)
+    } else if (controls.up.isDown) {
+      player.anims.play('walk_up', true)
+    } else {
+      console.log('walking sideways')
+      player.flipX = false
+      player.anims.play('walk_sideways', true)
+    }
+  } else {
+    body.setVelocityX(0)
+  }
+  if (controls.space.isDown) {
+    console.log('playing attack')
+    attacking = true
+    player.anims.play('attack', true)
+  }
 }
 
 export default controls
